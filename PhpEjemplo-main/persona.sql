53b396f430f097a5b0c312c5a318b988a5d08464
leonardomonzon <164917697+conejobasketsuma99@users.noreply.github.com>
2024-08-19 17:44:39 -0300

    a

--- PhpEjemplo-main/persona.sql
+++ PhpEjemplo-main/persona.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-10-2023 a las 01:54:19
--- Versión del servidor: 10.4.22-MariaDB
--- Versión de PHP: 8.1.2
+-- Tiempo de generación: 19-08-2024 a las 21:54:31
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,45 +18,79 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `gestionsubir`
+-- Base de datos: `germon`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `persona`
+-- Estructura de tabla para la tabla `dueño`
 --
 
-CREATE TABLE `persona` (
-  `id` int(4) NOT NULL,
-  `apellido` varchar(50) NOT NULL,
+CREATE TABLE `dueño` (
+  `IDduenio` int(11) NOT NULL,
   `nombre` varchar(50) NOT NULL,
-  `fecha` date NOT NULL,
-  `foto` longblob NOT NULL,
-  `usuario` varchar(50) NOT NULL,
-  `clave` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `Telefono` bigint(10) NOT NULL,
+  `Correo` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mascotas`
+--
+
+CREATE TABLE `mascotas` (
+  `IDmascota` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `raza` varchar(50) NOT NULL,
+  `peso` varchar(25) NOT NULL,
+  `edad` mediumint(40) NOT NULL,
+  `IDduenio` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `persona`
+-- Indices de la tabla `dueño`
 --
-ALTER TABLE `persona`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `usuario` (`usuario`);
+ALTER TABLE `dueño`
+  ADD PRIMARY KEY (`IDduenio`);
+
+--
+-- Indices de la tabla `mascotas`
+--
+ALTER TABLE `mascotas`
+  ADD PRIMARY KEY (`IDmascota`),
+  ADD KEY `Veterinaria` (`IDduenio`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `persona`
+-- AUTO_INCREMENT de la tabla `dueño`
 --
-ALTER TABLE `persona`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `dueño`
+  MODIFY `IDduenio` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `mascotas`
+--
+ALTER TABLE `mascotas`
+  MODIFY `IDmascota` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `mascotas`
+--
+ALTER TABLE `mascotas`
+  ADD CONSTRAINT `Veterinaria` FOREIGN KEY (`IDduenio`) REFERENCES `dueño` (`IDduenio`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
